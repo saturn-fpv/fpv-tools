aad12b845352ba2d9f648753c8b65f62aeff0a92
Saturn FPV <61454345+saturn-fpv@users.noreply.github.com>
2026-07-02 19:12:37 -0600

    release: bump version to 1.1.0 (code 4)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.saturnfpv.fpvtools"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.1.0"
         manifestPlaceholders["appName"] = "FPV Tools"
     }
 
